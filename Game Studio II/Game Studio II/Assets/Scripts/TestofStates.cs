ada1bce3954e2c0aa9cb762e3297df907a513068
Nominal9977 <84743651+Nominal9977@users.noreply.github.com>
2026-01-21 14:21:02 -0500

    working on small changes

--- Game Studio II/Game Studio II/Assets/Scripts/TestofStates.cs
+++ Game Studio II/Game Studio II/Assets/Scripts/TestofStates.cs
@@ -2,10 +2,13 @@ using UnityEngine;
 
 public class TestofStates : MonoBehaviour
 {
-   
+   StateMachine stateMachine;
+
     void Start()
     {
-        
+       StateMachine stateMachine = new StateMachine();
+       stateMachine.StartStateWithAuto<TestofStates>(this);
+        stateMachine.addGlobalTrans(sStartState, this.enabled = true);
     }
 
     void Update()
