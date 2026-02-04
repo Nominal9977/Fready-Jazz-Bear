a4bfb926292f2a01da31b6abd662b46eaacafaa7
Nominal9977 <84743651+Nominal9977@users.noreply.github.com>
2026-02-04 14:34:50 -0500

    testing removing fmod

--- GameStudio/Assets/Editor/BuildScript.cs
+++ GameStudio/Assets/Editor/BuildScript.cs
@@ -5,14 +5,22 @@ using System.IO;
 public class BuildScript
 {
     [MenuItem("Build/WebGL")]
-    public static void BuildWebGL()
+    public static void BuildWeb()
     {
+        // Get all enabled scenes
         string[] scenes = EditorBuildSettings.scenes
-            .Where(s => s.enabled)
-            .Select(s => s.path)
+            .Where(scene => scene.enabled)
+            .Select(scene => scene.path)
             .ToArray();
 
-        string buildPath = Path.Combine(Application.dataPath, "../build/WebGL");
+        if (scenes.Length == 0)
+        {
+            Debug.LogError("No scenes are enabled in Build Settings!");
+            return;
+        }
+
+        string buildPath = Path.Combine(Application.dataPath, "../builds/WebGL");
+        Directory.CreateDirectory(buildPath);
 
         BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions
         {
@@ -22,6 +30,15 @@ public class BuildScript
             options = BuildOptions.None
         };
 
-        BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+
+        if (report.summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("WebGL Build Succeeded!");
+        }
+        else
+        {
+            Debug.LogError("WebGL Build Failed!");
+        }
     }
 }
